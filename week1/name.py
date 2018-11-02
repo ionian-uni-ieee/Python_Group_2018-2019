37c72ab0047112b4e6d8821d35a033e4d8fa60ac
spirosbax <sprbax@gmail.com>
2018-11-02 09:05:10 +0200

    update week1

--- week1/name.py
+++ week1/name.py
@@ -1,4 +1,12 @@
 #5 apo https://www.w3resource.com/python-exercises/python-basic-exercises.php
-first_name = input()
-last_name = input()
-print(last_name + " " + first_name)
+# print("dwse onoma")
+# first_name = input("dwse onoma\n")
+# last_name = input("dwse last\n")
+# print(last_name, first_name)
+# fullname = input("give your name\n").split(' ')
+# fullname.append("faidwn")
+# a = [1,2,3,4,5]
+# for item in a[:3]:
+#     print(" ", item+1)
+for number in range(100, 5):
+    print(" ", number)
